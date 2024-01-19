e11b4ba565780fd96554470567ba53b3dcee0ee7
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-18 20:36:53 -0500

    docs(api): add documentation to the section categories endpoint

--- get_section_categories.php
+++ get_section_categories.php
@@ -1,8 +1,13 @@
 <?php
-
+/**
+ * Register REST Section Categories.
+ */
 define('SECTION_CATEGORY_IDS', [35, 3, 36, 5, 7, 37, 4]); // Parent categories
 define('DEPARTMENT_CATEGORY_IDS', [39]);
 
+/**
+ * Register REST API route for section categories.
+ */
 add_action('rest_api_init', 'register_rest_section_categories');
 
 function register_rest_section_categories() {
@@ -12,6 +17,12 @@ function register_rest_section_categories() {
     ]);
 }
 
+/**
+ * Callback function for handling section categories REST API requests.
+ *
+ * @param array $data The request data.
+ * @return WP_REST_Response|array Response data.
+ */
 function rest_section_categories_callback($data) {
     $town_category = isset($data['t']) ? absint($data['t']) : '';
     $page_number   = isset($data['p']) ? absint($data['p']) : 1;
@@ -39,6 +50,11 @@ function rest_section_categories_callback($data) {
     return rest_ensure_response($child_categories_with_posts);
 }
 
+/**
+ * Retrieve section child categories.
+ *
+ * @return array Array of section child categories.
+ */
 function get_section_child_categories() {
     $section_child_categories = [];
 
@@ -50,6 +66,13 @@ function get_section_child_categories() {
     return $section_child_categories;
 }
 
+/**
+ * Retrieve child categories with posts.
+ *
+ * @param array  $section_child_categories Array of section child categories.
+ * @param string $town_category           Town category.
+ * @return array Array of child categories with posts.
+ */
 function get_child_categories_with_posts($section_child_categories, $town_category) {
     $child_categories_with_posts = [];
 
@@ -77,8 +100,18 @@ function get_child_categories_with_posts($section_child_categories, $town_catego
     return $child_categories_with_posts;
 }
 
+/**
+ * Hook to delete transient when a post is updated.
+ *
+ * @param int $post_id Post ID.
+ */
 add_action('save_post', 'delete_child_categories_with_posts_transient');
 
+/**
+ * Delete transient when a post is updated.
+ *
+ * @param int $post_id Post ID.
+ */
 function delete_child_categories_with_posts_transient($post_id) {
     if (!defined('DEPARTMENT_CATEGORY_IDS')) {
         return;
